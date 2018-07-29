088e2e6981fc423616e567d9627fcf3144c02b49
Andres <Andres@DESKTOP-1OQLD32>
2018-07-28 22:13:23 -0500

    chupila

---
+++ BolsaDeMercado/src/main/java/com/bolsademercado/app/BolsaDeMercadoApplication.java
@@ -0,0 +1,12 @@
+package com.bolsademercado.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolsaDeMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolsaDeMercadoApplication.class, args);
+	}
+}

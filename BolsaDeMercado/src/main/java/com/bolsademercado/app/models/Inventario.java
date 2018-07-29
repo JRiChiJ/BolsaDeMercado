1fdcb3ad4e0e00f77f157e9e12321193e569275f
LuisMiguelCopa <34032102+LuisMiguelCopa@users.noreply.github.com>
2018-07-29 01:23:10 -0400

    inventario y puesto

---
+++ BolsaDeMercado/src/main/java/com/bolsademercado/app/models/Inventario.java
@@ -0,0 +1,37 @@
+package com.bolsademercado.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "inventarios")
+public class Inventario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long inventarioId;
+    private Long medidaId;
+    private Long detalleId;
+	public Long getInventarioId() {
+		return inventarioId;
+	}
+	public void setInventarioId(Long inventarioId) {
+		this.inventarioId = inventarioId;
+	}
+	public Long getMedidaId() {
+		return medidaId;
+	}
+	public void setMedidaId(Long medidaId) {
+		this.medidaId = medidaId;
+	}
+	public Long getDetalleId() {
+		return detalleId;
+	}
+	public void setDetalleId(Long detalleId) {
+		this.detalleId = detalleId;
+	}
+}
+
+
---
+++ BolsaDeMercado/src/main/java/com/bolsademercado/app/models/Puesto.java
@@ -0,0 +1,47 @@
+package com.bolsademercado.app.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "puestos")
+public class Puesto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long puestoId;
+	
+	private Long establecimientoId;
+	private Long duenoId;
+	private int status;
+	
+	public Long getPuestoId() {
+		return puestoId;
+	}
+	public void setPuestoId(Long puestoId) {
+		this.puestoId = puestoId;
+	}
+	public Long getEstablecimientoId() {
+		return establecimientoId;
+	}
+	public void setEstablecimientoId(Long establecimientoId) {
+		this.establecimientoId = establecimientoId;
+	}
+	public Long getDuenoId() {
+		return duenoId;
+	}
+	public void setDuenoId(Long duenoId) {
+		this.duenoId = duenoId;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+		
+
+}
